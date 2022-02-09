5ccf8ecea8a4576ceaa607a41cae9576d7b42b61
anthonyshadow <anthony.shadowitz@gmail.com>
2022-02-09 11:39:00 -0500

    Parse through results and send items links/names and href

--- main.py
+++ main.py
@@ -2,10 +2,35 @@ from bs4 import BeautifulSoup
 import requests
 
 
-# Scrapes the requested webpage for your term and then gives you data properly organized
-search = input("Enter Search Term: ")
+# Scrapes the requested webpage for your term and then gives you data properly organized usiing beautifulsoup
+
+
+# search = input("Enter Search Term: ")
+# params = {"q": search}
+# r = requests.get("https://www.bing.com/search", params=params)
+
+# soup = BeautifulSoup(r.text)
+# print(soup.prettify())
+
+
+# ---------------------------------------------------------------------------------------------
+
+
+# Parsing our soup
+
+search = input("Search for: ")
 params = {"q": search}
 r = requests.get("https://www.bing.com/search", params=params)
 
-soup = BeautifulSoup(r.text)
-print(soup.prettify())
+soup = BeautifulSoup(r.text, "html.parser")
+
+results = soup.find("ol", {"id": "b_results"})
+links = results.findAll("li", {"class": "b_algo"})
+
+for item in links:
+    item_text = item.find("a").text
+    item_href = item.find("a").attrs["href"]
+
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
