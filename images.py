9c9f7e2560c03d79232a9338846365461c9fbc86
anthonyshadow <anthony.shadowitz@gmail.com>
2022-02-09 12:28:16 -0500

    imporve Image webscrapper by adding loop and creating new directories

--- images.py
+++ images.py
@@ -2,25 +2,46 @@ from bs4 import BeautifulSoup
 import requests
 from PIL import Image
 from io import BytesIO
+import os
+
+#Making it loopable
+
+def StartSearch(): 
 
 # Saving Images from bing based on search
 
-search = input("Search For: ")
-params = {"q": search}
-r = requests.get("http://www.bing.com/images/search", params=params)
+    search = input("Search For: ")
+    params = {"q": search}
+    dir_name = search.replace(" ", "_").lower()
 
-soup = BeautifulSoup(r.text, "html.parser")
-links = soup.findAll("a", {"class": "thumb"})
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name) 
 
-for item in links:
-    img_obj = requests.get(item.attrs["href"])
-    print("Getting", item.attrs["href"])
-    # To get the last in a list in python use -1
-    title = item.attrs["href"].split("/")[-1]
-    img = Image.open(BytesIO(img_obj.content))
+    r = requests.get("http://www.bing.com/images/search", params=params)
 
-    # Saves scraped images to new directory
-    path = "./scraped_images/"
-    img.save(path + title, img.format)
+    soup = BeautifulSoup(r.text, "html.parser")
+    links = soup.findAll("a", {"class": "thumb"})
+
+    for item in links:
+        try: 
+            img_obj = requests.get(item.attrs["href"])
+            print("Getting", item.attrs["href"])
+            # To get the last in a list in python use -1
+            title = item.attrs["href"].split("/")[-1]
+            try:
+                img = Image.open(BytesIO(img_obj.content))
+
+                # Saves scraped images to new directory
+                img.save("./" + dir_name + title, img.format)
+            except:
+                print("Could not save image.")
+        
+        except:
+            print("Could not request image")
+            
+        StartSearch()
+
+StartSearch()
+
 
 
